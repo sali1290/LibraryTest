1f1165f6caef8fa589bd9074793a02b4800b0c4e
Ali Nabavi <arz1379n@gmail.com>
2024-02-08 12:10:56 +0330

    add string xml

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,8 +3,6 @@
 0Test/src/main/java/com/example/test/MainClass.kt,0\a\0a86a7ca0a00a9f89debe2b60e176760b040c130
 E
 Test/build.gradle.kts,1\f\1fe3f46ef0fd78f49f232848e1e2179c4635cf42
-;
-jitpack.yml,3\f\3f384c5ac91d01ea9ab11981ab87f1d958f63284
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
--- Test/src/main/java/com/example/test/MainClass.kt
+++ Test/src/main/java/com/example/test/MainClass.kt
@@ -1,10 +1,11 @@
 package com.example.test
 
+import android.content.Context
 import android.util.Log
 import android.widget.Toast
 
 class MainClass {
-    fun mainFun() {
-        Log.i("Library log", "This is a test from library")
+    fun mainFun(context: Context) {
+        Log.i("Library log", context.getString(R.string.test_text))
     }
 }
